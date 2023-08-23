6a7e79d6c850682ffc750964b2abe1b5b47032af
Reginaldo Florencio de Paiva Filho <2092563@ad.ifpb.edu.br>
2023-08-23 08:31:31 -0300

    Imagens do menu adicionadas

--- src/componentes/Navegacao.js
+++ src/componentes/Navegacao.js
@@ -12,8 +12,8 @@ function Navegacao(){
                         <a className='icone' href="#">Manuais</a></li>
                     <li><img className='imagem__menu' src="./imagem/acton-crawford-O2Q_b_NN6qw-unsplash.jpg"/>
                     <a  className='icone' href="#">Manutenção Preventiva e Corretiva</a></li>
-                    <li><a  className='icone' href="#">Equipamentos</a></li>
-                    <li><a  className='icone' href="#">Estoque e Insumos</a></li>
+                    <li><img className='imagem__menu' src="./imagem/IMG_1372.jpg"/><a  className='icone' href="#">Equipamentos</a></li>
+                    <li><img className='imagem__menu' src="./imagem/IMG_1379.jpg"/><a  className='icone' href="#">Estoque e Insumos</a></li>
                 </ul>
            
         </section>
